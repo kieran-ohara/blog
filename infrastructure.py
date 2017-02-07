fccff5766c06ddd91935b1c1b0c2bbbd747443c6
kieranbamforth <hello@kieranbamforth.me>
2017-02-07 23:48:53 +0000

    Removing cloudformation.json as it has been replaced by infrastructure.py

--- infrastructure.py
+++ infrastructure.py
@@ -1,5 +1,14 @@
 from troposphere import Template
+import troposphere.s3 as s3
 
 template = Template()
+
+bucketNames = ['kieranbamforth.me', 'www.kieranbamforth.me']
+
+for index, bucketName in enumerate(bucketNames):
+    resourceName = 'bucket{}'.format(index)
+    bucket = s3.Bucket(resourceName)
+    bucket.BucketName = bucketName
+    template.add_resource(bucket)
 
 print(template.to_json())
