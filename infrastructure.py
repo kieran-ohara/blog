2fe1485b6dd6e00697ad8bd93aa8e4a41dfb9c60
kieranbamforth <hello@kieranbamforth.me>
2017-02-08 00:09:40 +0000

    Adding public access to both buckets in infrastructure.py.

--- infrastructure.py
+++ infrastructure.py
@@ -6,9 +6,26 @@ template = Template()
 bucketNames = ['kieranbamforth.me', 'www.kieranbamforth.me']
 
 for index, bucketName in enumerate(bucketNames):
-    resourceName = 'bucket{}'.format(index)
-    bucket = s3.Bucket(resourceName)
+    bucketResourceName = 'bucket{}'.format(index)
+    bucket = s3.Bucket(bucketResourceName)
     bucket.BucketName = bucketName
     template.add_resource(bucket)
+
+    bucketPolicyDocument = {
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Sid": "Allow Public Access to All Objects",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::{}/*".format(bucketName)
+                }]
+            }
+
+    bucketPolicyResourceName = 'bucketPolicy{}'.format(index)
+    bucketPolicy = s3.BucketPolicy(bucketPolicyResourceName)
+    bucketPolicy.Bucket = bucketName
+    bucketPolicy.PolicyDocument = bucketPolicyDocument
+    template.add_resource(bucketPolicy)
 
 print(template.to_json())
