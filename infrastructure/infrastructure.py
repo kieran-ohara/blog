66333eaf6f960223b5402bcebdd0e49a5cce7d64
Kieran Bamforth <hello@kieranbamforth.me>
2018-02-20 00:10:01 +0000

    untangling the infrastructure.py

--- infrastructure/infrastructure.py
+++ infrastructure/infrastructure.py
@@ -7,72 +7,60 @@ from troposphere.cloudfront import S3Origin
 import troposphere.s3 as s3
 import troposphere.route53 as r53
 
-import pdb
-
 template = Template()
 
-bucketNames = ['kieranbamforth.me', 'www.kieranbamforth.me']
-hostedZoneId = 'Z36PFUEKT1FWSA'
+content_bucket = template.add_resource(s3.Bucket(
+    'bucket1',
+    BucketName='www.kieranbamforth.me',
+    WebsiteConfiguration=s3.WebsiteConfiguration(
+        IndexDocument='index.html',
+        ErrorDocument='blog/404.html'
+        )
+    ))
 
-for index, bucketName in enumerate(bucketNames):
-    bucketWebsiteConfiguration = s3.WebsiteConfiguration()
-    if index == 0:
-        redirect = s3.RedirectAllRequestsTo()
-        redirect.HostName = 'www.kieranbamforth.me'
-        redirect.Protocol = 'http'
-        bucketWebsiteConfiguration.RedirectAllRequestsTo = redirect
-    elif index == 1:
-        bucketWebsiteConfiguration.IndexDocument = 'index.html'
-        bucketWebsiteConfiguration.ErrorDocument = 'blog/404.html'
+content_bucket_poilcy = template.add_resource(s3.BucketPolicy(
+    'bucketPolicy1',
+    Bucket='www.kieranbamforth.me',
+    PolicyDocument={
+        'Version': '2012-10-17',
+        'Statement': [{
+            'Sid': 'Allow Public Access to All Objects',
+            'Effect': 'Allow',
+            'Principal': '*',
+            'Action': 's3:GetObject',
+            'Resource': 'arn:aws:s3:::www.kieranbamforth.me/*'
+            }]
+        }
+    ))
 
-    bucketResourceName = 'bucket{}'.format(index)
-    bucket = s3.Bucket(bucketResourceName)
-    bucket.BucketName = bucketName
-    bucket.WebsiteConfiguration = bucketWebsiteConfiguration
-    template.add_resource(bucket)
+redirect_bucket = template.add_resource(s3.Bucket(
+    'bucket0',
+    BucketName='kieranbamforth.me',
+    WebsiteConfiguration=s3.WebsiteConfiguration(
+        RedirectAllRequestsTo=s3.RedirectAllRequestsTo(
+            HostName='www.kieranbamforth.me',
+            Protocol='http'
+            )
+        )
+    ))
 
-    if index == 1:
-        bucketPolicyDocument = {
-                'Version': '2012-10-17',
-                'Statement': [{
-                    'Sid': 'Allow Public Access to All Objects',
-                    'Effect': 'Allow',
-                    'Principal': '*',
-                    'Action': 's3:GetObject',
-                    'Resource': 'arn:aws:s3:::{}/*'.format(bucketName)
-                    }]
-                }
-
-        bucketPolicyResourceName = 'bucketPolicy{}'.format(index)
-        bucketPolicy = s3.BucketPolicy(bucketPolicyResourceName)
-        bucketPolicy.Bucket = bucketName
-        bucketPolicy.PolicyDocument = bucketPolicyDocument
-
-        template.add_resource(bucketPolicy)
-
-    if index == 0:
-        aliasTarget = r53.AliasTarget()
-        aliasTarget.HostedZoneId = 'Z1BKCTXD74EZPE'
-        aliasTarget.DNSName = 's3-website-eu-west-1.amazonaws.com'
-
-        recordSetResourceName = 'recordSetApex'
-        recordSet = r53.RecordSetType(recordSetResourceName)
-        recordSet.AliasTarget = aliasTarget
-        recordSet.HostedZoneId = hostedZoneId
-        recordSet.Name = bucketName + '.'
-        recordSet.Type = 'A'
-
-        template.add_resource(recordSet)
+redirect_record_set = template.add_resource(r53.RecordSetType(
+    'recordSet0',
+    AliasTarget=r53.AliasTarget(
+        HostedZoneId='Z1BKCTXD74EZPE',
+        DNSName='s3-website-eu-west-1.amazonaws.com'),
+    HostedZoneName='kieranbamforth.me.',
+    Name='kieranbamforth.me.',
+    Type='A'
+    ))
 
 cloudfront_dist = template.add_resource(Distribution(
     'cloudfrontDistribution',
     DistributionConfig=DistributionConfig(
-        Aliases=[
-            'www.kieranbamforth.me'
-            ],
+        Aliases=['www.kieranbamforth.me'],
         Origins=[Origin(
             Id='S3Origin',
-            DomainName=GetAtt(template.resources['bucket1'], 'DomainName'),
+            DomainName=GetAtt(content_bucket, 'DomainName'),
             S3OriginConfig=S3Origin())],
         DefaultCacheBehavior=DefaultCacheBehavior(
             TargetOriginId='S3Origin',
@@ -88,7 +76,7 @@ cloudfront_record_set = template.add_resource(r53.RecordSetType(
     AliasTarget=r53.AliasTarget(
         HostedZoneId='Z2FDTNDATAQYW2',
         DNSName=GetAtt(cloudfront_dist, 'DomainName')),
-    HostedZoneId=hostedZoneId,
+    HostedZoneName='kieranbamforth.me.',
     Name='www.kieranbamforth.me.',
     Type='A'
     ))
