a87fe03b1513f810b722f45adb7c652e2931cf25
Kieran Bamforth <hello@kieranbamforth.me>
2018-05-20 19:55:44 +0100

    Adding sitemap (first attempt).
    Signed-off-by: Kieran Bamforth <hello@kieranbamforth.me>

---
+++ submit_sitemap.py
@@ -0,0 +1,41 @@
+import pdb
+
+import httplib2
+import os
+
+from apiclient import errors
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+
+# Copy your credentials from the console
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+pdb.set_trace()
+# Check https://developers.google.com/webmaster-tools/search-console-api-original/v3/ for all available scopes
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/webmasters'
+
+# Redirect URI for installed apps
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+# Run through the OAuth flow and retrieve credentials
+flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+authorize_url = flow.step1_get_authorize_url()
+print 'Go to the following link in your browser: ' + authorize_url
+code = raw_input('Enter verification code: ').strip()
+credentials = flow.step2_exchange(code)
+
+# Create an httplib2.Http object and authorize it with our credentials
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+webmasters_service = build('webmasters', 'v3', http=http)
+
+# Retrieve list of properties in account
+site_list = webmasters_service.sites().list().execute()
+
+# Printing the URLs of all websites you are verified for.
+sitemaps = webmasters_service.sitemaps().submit(
+    siteUrl='https://www.kieranbamforth.me',
+    feedpath='https://www.kieranbamforth.me/blog/sitemap.xml'
+).execute()
