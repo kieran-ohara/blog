6fb8590ccff23ad8e05ff8e12611210fa31d8855
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-06 00:22:13 +0100

    Rename CSS to something more apt.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import './reset.css';
-import './fonts.scss';
+import './styles.scss';
 
 export default function Layout({ children }) {
   return (
