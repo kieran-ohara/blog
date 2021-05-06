3c8d9d1b90cc9787f0aeb6510bc713071a4fd013
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-06 08:43:19 +0100

    Add helment for setting title.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import './reset.css';
 import './styles.scss';
+import { Helmet } from 'react-helmet';
 
 export default function Layout({ children }) {
   return (
@@ -17,6 +18,11 @@ export default function Layout({ children }) {
         `}
       render={data => (
         <div>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>{data.site.siteMetadata.title}</title>
+            <link rel="canonical" href="https://www.kieranbamforth.me/" />
+          </Helmet>
           <header>
             <h1>{data.site.siteMetadata.title}</h1>
             <>{children}</>
