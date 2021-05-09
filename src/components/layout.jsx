2cfd9f97cce2eac152a7ddb7744865495a3e777b
Kieran Bamforth <hello@kieranbamforth.me>
2021-05-09 11:43:19 +0100

    Preload the body/title fonts.
    Swap the non-common fonts in.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,8 +3,37 @@ import { StaticQuery, graphql } from 'gatsby';
 import './reset.css';
 import './styles.scss';
 import { Helmet } from 'react-helmet';
+import VolkornMd from '@fontsource/vollkorn/files/vollkorn-latin-500-normal.woff2';
+import LatoRg from '@fontsource/lato/files/lato-latin-400-normal.woff2';
 
 export default function Layout({ children }) {
+  const render = (data) => (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{data.site.siteMetadata.title}</title>
+        <link rel="canonical" href="https://www.kieranbamforth.me/" />
+        <link
+          rel="preload"
+          as="font"
+          href={VolkornMd}
+          type="font/woff2"
+          crossOrigin
+        />
+        <link
+          rel="preload"
+          as="font"
+          href={LatoRg}
+          type="font/woff2"
+          crossOrigin
+        />
+      </Helmet>
+      <header>
+        <h1>{data.site.siteMetadata.title}</h1>
+        <>{children}</>
+      </header>
+    </div>
+  );
   return (
     <StaticQuery
       query={graphql`
@@ -16,19 +45,7 @@ export default function Layout({ children }) {
           }
         }
         `}
-      render={data => (
-        <div>
-          <Helmet>
-            <meta charSet="utf-8" />
-            <title>{data.site.siteMetadata.title}</title>
-            <link rel="canonical" href="https://www.kieranbamforth.me/" />
-          </Helmet>
-          <header>
-            <h1>{data.site.siteMetadata.title}</h1>
-            <>{children}</>
-          </header>
-        </div>
-      )}
+      render={render}
     />
   );
-};
+}
